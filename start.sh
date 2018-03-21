e015f6d3e03849bd62fffa8f339a3c76e31bb010
Maxime Griot <maxime@griot.fr>
2018-03-21 18:40:11 +0100

    Don't chown on startup

--- start.sh
+++ start.sh
@@ -4,8 +4,5 @@
 procs=$(cat /proc/cpuinfo |grep processor | wc -l)
 sed -i -e "s/worker_processes  1/worker_processes $procs/" /etc/nginx/nginx.conf
 
-# Always chown webroot for better mounting
-chown -Rf nginx.nginx /usr/share/nginx/html
-
 # Start supervisord and services
 /usr/local/bin/supervisord -n -c /etc/supervisord.conf
